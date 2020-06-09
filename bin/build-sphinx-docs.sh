5109cb60962eb799c0cb6e5d07664955358f4cd8
jonathan-winn-geo <30663163+jonathan-winn-geo@users.noreply.github.com>
2020-06-09 14:03:33 +0100

    Update build-sphinx-docs.sh

--- bin/build-sphinx-docs.sh
+++ bin/build-sphinx-docs.sh
@@ -11,7 +11,7 @@ echo "$INPUT_DIR"
 OUTPUT_DIR=$CODE_DIR/docs/build
 echo "$OUTPUT_DIR"
 
-# Generate source files from current range of installed HadCRUT package / subpackages
+# Generate source files from current range of installed packages / subpackages
 sphinx-apidoc -f -o $INPUT_DIR $CODE_DIR/cmascience
 
 # Build sphinx docs and make html
