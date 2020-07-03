4ae1d931284dbde1abf0a3c6428ae2d5dd9ce1d1
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-07-03 12:54:10 +0100

    Adds extra test netcdf files

--- bin/view-netcdf-metadata.sh
+++ bin/view-netcdf-metadata.sh
@@ -7,7 +7,16 @@ DATA=$CODE_DIR/data/reference
 
 TEST_FILE="tos_O1_2001-2002.nc"
 TEST_FILE_2="sresa1b_ncar_ccsm3-example.nc"
+TEST_FILE_3="HadSST.4.0.0.0_median.nc"
 
 # ncdump -c $DATA/$TEST_FILE
 
 ncdump -c $DATA/$TEST_FILE
+
+echo "------"
+
+ncdump -c $DATA/$TEST_FILE_2
+
+echo "------"
+
+ncdump -c $DATA/$TEST_FILE_3
