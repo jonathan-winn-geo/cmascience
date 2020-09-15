0cfda0802e27db1b5c35f097670d13c6fccf2f7c
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-15 14:39:14 +0100

    Updates to include uninstall of cmatools

--- bin/uninstall-cmascience.sh
+++ bin/uninstall-cmascience.sh
@@ -5,8 +5,11 @@
 
 CODE_DIR="$(dirname "$PWD")"
 echo "$CODE_DIR"
+
 pip uninstall -y cmascience
-echo "Removing .egg-info directory"
+pip uninstall -y cmatools
+
 rm -rf $CODE_DIR/build
 echo "Removing build directory"
 rm -rf $CODE_DIR/cmascience.egg-info
+echo "Removing .egg-info directory"
