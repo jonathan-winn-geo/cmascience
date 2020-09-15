8e1c795b06bdd6a247ad07acd2bfdca21574e6ef
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-15 14:42:03 +0100

    Updates to setup to include install of cmatools with install_requires

--- setup.py
+++ setup.py
@@ -11,19 +11,25 @@ setuptools.setup(
     description="Training example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jonathan-winn-geo/cmascience",
+    url="https://github.com/cma-open/cmascience",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         #"License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        #"Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
     include_package_data = True,
     package_data = {
         # Ensure config.ini file is available within installed package
-    # If any package contains *.ini files, include them
+        # If any package contains *.ini files, include them
         '': ['*.ini']
-    }
+    },
+    install_requires=[
+                      "cmatools @ git+https://github.com/cma-open/cmatools",
+                     # "package2 @ git+https://github.com/user2/package2@branch1",
+
+                      ],
+
     # entry_points
 )
