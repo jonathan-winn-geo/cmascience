bb071aafc6b872d5e2e787204eca34726836bdff
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-15 14:40:45 +0100

    Adds test for cmatools import

---
+++ tests/unit/cmatools/test_cmatools.py
@@ -0,0 +1,18 @@
+import sys
+
+def test_cmatools_installed():
+
+    """
+    Test to verify that the cmatools package has been installed and can be imported
+    """
+
+
+    try:
+        import cmatools
+    except ImportError as e:
+        print('cmatools was not available for import')
+        print(e)
+        print('Try re-installing the system, ensure cmatools package is available')
+
+    assert ('cmatools' in sys.modules) is True
+
