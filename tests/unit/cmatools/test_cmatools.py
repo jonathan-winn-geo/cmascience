fe51383333554e324c4849a4436274d9c29c603e
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-16 12:53:54 +0100

    Code style edits from Codacy

--- tests/unit/cmatools/test_cmatools.py
+++ tests/unit/cmatools/test_cmatools.py
@@ -2,14 +2,13 @@ import sys
 
 
 def test_cmatools_installed():
-
-    """    Test to verify that the cmatools package has been installed and can be imported """
+    """ Test to verify that the cmatools package has been installed and can be imported """
 
     try:
         import cmatools
+        print(f'cmatools version: {cmatools.__version__}')
     except ImportError as e:
-        print("cmatools was not available for import")
+        print('cmatools was not available for import')
         print(e)
-        print("Try re-installing the system, ensure cmatools package is available")
-
-    assert ("cmatools" in sys.modules) is True
+        print('Try re-installing the system, ensure cmatools package is available')
+    assert ('cmatools' in sys.modules) is True
