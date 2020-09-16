e7223004d6f04aab326653c6020ab2711dc5b86a
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-16 12:16:03 +0100

    Code style edits from Codacy

--- bin/format-code-black.sh
+++ bin/format-code-black.sh
@@ -3,11 +3,11 @@
 codedir="$(dirname "$PWD")"
 echo "-----------"
 echo "Code directory root for formatting"
-echo "$codedir"
-echo $codedir/cmascience
-echo $codedir/tests
+echo "$codedir" # reminder: Canonical quoting
+echo "$codedir/cmascience"
+echo "$codedir/tests"
 echo "-----------"
 
 # run black autoformatting against named directories
-black $codedir/cmascience
-black $codedir/tests
+black "$codedir/cmascience"
+black "$codedir/tests"
