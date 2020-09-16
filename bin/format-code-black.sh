81d00a1b9536e2f7d2190a0686210942de26028b
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-09-16 11:50:34 +0100

    Updates black code format script, more output to the user

--- bin/format-code-black.sh
+++ bin/format-code-black.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 codedir="$(dirname "$PWD")"
+echo "-----------"
+echo "Code directory root for formatting"
 echo "$codedir"
+echo $codedir/cmascience
+echo $codedir/tests
+echo "-----------"
 
+# run black autoformatting against named directories
 black $codedir/cmascience
 black $codedir/tests
