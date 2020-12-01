599de47f491acd3099e2b6425052b62f9a02b10d
jonathan.winn <jonathan.winn@metoffice.gov.uk>
2020-12-01 11:29:56 +0000

    Reorganises and renames test scripts

---
+++ bin/tests/unit-cli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rootdir="$(dirname $(dirname "$PWD"))"
+testdir="$rootdir/tests/unit/cli"
+
+echo "----------"
+echo "$rootdir"
+echo "$testdir"
+echo "----------"
+
+
+# Discover and run tests on code path,  -v verbose flag
+#python3 -m pytest -v --capture=tee-sys  $testdir
+pytest -v --capture=tee-sys -rA  $testdir
---
+++ bin/tests/user-interface.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rootdir="$(dirname $(dirname "$PWD"))"
+testdir="$rootdir/tests/user-interface"
+
+echo "----------"
+echo "$rootdir"
+echo "$testdir"
+echo "----------"
+
+# Discover and run tests on code path,  -v verbose flag
+#python3 -m pytest -v --capture=tee-sys  $testdir
+pytest -v --capture=tee-sys -rA  $testdir
